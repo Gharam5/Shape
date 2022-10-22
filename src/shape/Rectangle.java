5beb2b17a53594cca6cdea38d6bf7dd443a336da
gharamalshammri <gharamalshammri@localhost>
2022-10-22 15:53:51 +0300

    modify done add method

--- src/shape/Rectangle.java
+++ src/shape/Rectangle.java
@@ -1,27 +1,1 @@
-package shape;
-
-/**
- *
- * @author
- */
-public class Rectangle {
-
-    private int length; 
-    private int breadth; 
-    
-    public Rectangle(int l, int b) {
-        length = l;
-        breadth = b;
-    }
-    
-    public int getArea() { 
-         return length*breadth;     //  calculate area here
-    } 
-    //// create method to find perimeter rectangle
-    public int getPerimeter() {
-        return 2*(length+breadth); 
-    }
-      ///// calculate the perimeters   
-     // getPerimeter()
-/// test
-}
+package shape;/** * * @author */public class Rectangle {    private int length;     private int breadth;         public Rectangle(int l, int b) {        length = l;        breadth = b;    }       public int getArea() {          return length*breadth;     //  calculate area here    }     ////g    //// create method to find perimeter rectangle    public int getPerimeter() {        return 2*(length+breadth);     }      ///// calculate the perimeters        // getPerimeter() }
